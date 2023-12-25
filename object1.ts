61edd16c480917f225d98eb058d2c5b48f001c48
igorcuric22 <igor.curic55@gmail.com>
2023-12-25 20:51:19 +0100

    object constructor

--- object1.ts
+++ object1.ts
@@ -44,3 +44,15 @@ let vacant: {} = {};
 
 console.log(vacant.toString());
 
+class VirtualPoint {
+    x: number;
+    y: number;
+   
+    constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+   
+  const newVPoint = new VirtualPoint(13, 56);
+  console.log(newVPoint); // logs "13, 56"
