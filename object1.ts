c810c166a2231bf0f216d4a37cc2111c7171b15b
igorcuric22 <igor.curic55@gmail.com>
2023-12-25 18:04:48 +0100

    object

---
+++ object1.ts
@@ -0,0 +1,46 @@
+let employee: object;
+
+employee = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+console.log(employee);
+
+
+let employee1: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+};
+
+employee1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+console.log(employee1);
+
+let employee2: {
+    firstName: string;
+    lastName: string;
+    age: number;
+    jobTitle: string;
+} = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+    jobTitle: 'Web Developer'
+};
+
+console.log(employee2);
+
+let vacant: {} = {};
+
+console.log(vacant.toString());
+
