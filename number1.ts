2f59dea3d52100a62a48551c8819c5bea1c734a1
igorcuric22 <igor.curic55@gmail.com>
2023-12-25 17:28:31 +0100

    number

---
+++ number1.ts
@@ -0,0 +1,28 @@
+let counter: number = 7;
+
+console.log(counter);
+
+let x: number = 100, 
+    y: number = 200;
+
+
+console.log(x,y);
+
+console.log("=====");
+
+let bin = 0b100;
+let anotherBin: number = 0B010;
+
+console.log(bin,anotherBin);
+
+console.log("=====");
+
+let octal: number = 0o10;
+
+console.log(octal);
+
+console.log("=====");
+
+let hexadecimal: number = 0XA;
+
+console.log(hexadecimal);
