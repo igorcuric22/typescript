e8ea2c6770dcf7895f471ef16ccf0f845380099f
igorcuric22 <igor.curic55@gmail.com>
2023-12-25 13:04:22 +0100

    deklaracija varijabli

--- type1.ts
+++ type1.ts
@@ -14,3 +14,14 @@ function getProduct(id:number):Product {
 
 const product = getProduct(1);
 console.log(`The product ${product.name} costs $${product.price}`);
+
+
+const showProduct = (name: string, price:number)  => {
+  console.log(`The product ${name} costs $${price}.`);
+};
+
+const product1 = getProduct(1);
+// showProduct(product1.price, product1.name);
+
+showProduct(product1.name,product1.price);
+
---
+++ type2.ts
@@ -0,0 +1,23 @@
+let name: string = 'John';
+let age: number = 25;
+let active: boolean = true;
+
+console.log(`${name}  ${age} ${active}`)
+
+let names: string[] = ['John', 'Jane', 'Peter', 'David', 'Mary'];
+
+console.log(names);
+
+let person: {
+    name: string;
+    age: number
+ };
+ 
+ person = {
+    name: 'John',
+    age: 25
+ }; 
+
+ console.log(person);
+
+ 
