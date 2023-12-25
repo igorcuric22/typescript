aa1c8a4df7b5dbe5a1b382114c196170d3678554
igorcuric22 <igor.curic55@gmail.com>
2023-12-25 19:06:22 +0100

    array

---
+++ array1.ts
@@ -0,0 +1,12 @@
+let skills: string[];
+skills = ['Problem Sovling','Software Design','Programming'];
+
+
+console.log(skills);
+
+let skill:string[]=[];
+skill[0] = "Problem Solving";
+skill[1] = "Programming";
+skill.push('Software Design');
+
+console.log(skill);
