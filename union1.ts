2c52b3f3196dca7fa652ab3fe2778d24b5f8a657
igorcuric22 <igor.curic55@gmail.com>
2023-12-26 19:22:16 +0100

    number | string

--- union1.ts
+++ union1.ts
@@ -16,3 +16,20 @@ console.log(add(true,false));
     console.log(e.message);
 }
 
+function add1(a: number | string, b: number | string) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    if (typeof a === 'string' && typeof b === 'string') {
+        return a.concat(b);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
+
+try{
+    console.log(add1(5,2));
+    console.log(add1("5","2"));
+    console.log(add1(true,false));
+    } catch(e:any) {
+        console.log(e.message);
+    }
