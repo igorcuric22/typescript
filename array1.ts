36ecc9decabb3f189ba0d3df4c1b8b651d536b34
igorcuric22 <igor.curic55@gmail.com>
2023-12-26 16:43:51 +0100

    array and tuple

--- array1.ts
+++ array1.ts
@@ -12,3 +12,16 @@ skill.push('Software Design');
 console.log(skill);
 
 console.log(skill.length);
+
+console.log(typeof(skill));
+
+let series = [1, 2, 3];
+console.log(series.length); // 3
+
+let doubleIt = series.map(e => e* 2);
+console.log(doubleIt);
+
+
+let scores:(string | number)[] = ['Programming', 5, 'Software Design', 4]; 
+console.log(scores);
+
---
+++ tuple1.ts
@@ -0,0 +1,16 @@
+let skill: [string, number];
+skill = ['Programming', 5];
+
+console.log(skill);
+
+let color: [number, number, number] = [255, 0, 0];
+
+console.log(color);
+
+let bgColor, headerColor: [number, number, number, number?];
+bgColor = [0, 255, 255, 0.5];
+headerColor = [0, 255, 255];
+
+console.log(bgColor);
+console.log(headerColor);
+
