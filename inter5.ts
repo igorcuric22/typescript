5dd094d0ab3b678efc24ed7e046f13a06b2094d5
igorcuric22 <igor.curic55@gmail.com>
2023-12-29 09:23:17 +0100

    inter 5

---
+++ inter5.ts
@@ -0,0 +1,10 @@
+interface StringFormat {
+    (str: string, isUpper: boolean): string
+}
+
+let lowerCase: StringFormat;
+lowerCase = function (str: string) {
+    return str.toLowerCase();
+}
+
+console.log(lowerCase('Hi', false));
